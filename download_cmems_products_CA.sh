117e8acbf157576976d531a91e43978cff959e71
CarolinaAmadio <caio.spoleto@gmail.com>
2024-01-25 16:44:41 +0100

    added files to subset INSITU dataset from CMEMS via ftp

---
+++ download_cmems_products_CA.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#
+# The script read year(s) from standard input
+# The destination directory can be optionally specified on command line as first parameter
+#
+# Usage examples:
+#
+# To download year 2020 data
+# echo 2020 | ./download_cmems_products.sh
+#
+# To download data from year 2017 to year 2020
+# seq 2017 2020 | ./download_cmems_products.sh
+#
+# To download data into directory "/download_dir"
+# echo 2020 | ./download_cmems_products.sh download_dir/
+#
+
+####### CLS #################
+declare -r SITE="nrt.cmems-du.eu";
+declare -r USER="camadio";
+declare -r PASS="Bsqx3066!!";
+###################################
+
+declare DOWNLOAD_DIR="/g100_scratch/userexternal/camadio0/GL_CMEMS/CMEMS_013_035_GL_DATA";
+declare FTP_BASE_PATH="/Core/INSITU_MED_PHYBGCWAV_DISCRETE_MYNRT_013_035/cmems_obs-ins_med_phybgcwav_mynrt_na_irr/monthly/GL";
+declare FTP_indextxt_PATH="/Core/INSITU_MED_PHYBGCWAV_DISCRETE_MYNRT_013_035/cmems_obs-ins_med_phybgcwav_mynrt_na_irr/";
+
+if [ $# -ge 1 ]; then
+	DOWNLOAD_DIR="$1";
+fi
+
+echo ${DOWNLOAD_DIR}
+
+if [ ! -d "${DOWNLOAD_DIR}" ]; then
+	mkdir "${DOWNLOAD_DIR}" || { echo "Unable to create destination directory" >&2; exit 1; };
+fi
+
+
+while read YEAR
+do
+	seq 1 12 |\
+	while read MONTH
+	do
+
+		printf -v MONTH "%02d" ${MONTH};
+
+		FTP_URL="ftp://${SITE}${FTP_BASE_PATH}/${YEAR}${MONTH}/";
+                #echo $FTP_URL
+		# list files
+		curl \
+			--silent \
+			--user ${USER}:${PASS} \
+			--list-only \
+			${FTP_URL} |\
+		while read FILENAME
+		do
+			# download file
+			curl \
+				--silent \
+				--user ${USER}:${PASS} \
+				--output "${DOWNLOAD_DIR}/${FILENAME}" \
+				"${FTP_URL}${FILENAME}" \
+			;
+		done
+	done
+done
+
+FTP_URL_="ftp://${SITE}${FTP_indextxt_PATH}/"
+FILENAME_=index_monthly.txt
+curl \
+  --silent \
+  --user ${USER}:${PASS} \
+  --output "${DOWNLOAD_DIR}/${FILENAME_}" \
+  "${FTP_URL_}${FILENAME_}" \
+  ;
+
+exit 0;
+
