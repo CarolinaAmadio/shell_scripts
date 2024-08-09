a203d7ed9d4eaeb5e9ce89bcb884fc919adb5b3f
CarolinaAmadio <caio.spoleto@gmail.com>
2024-08-09 12:08:55 +0200

    Correction of data update in the loop and added the writing of both freq files

--- create_avetimes_file.sh
+++ create_avetimes_file.sh
@@ -5,7 +5,7 @@ end_date="20220101"
 
 #############  FREQ DAILY ############# 
 
-output_file="Daily_2.aveTimes"
+output_file="Daily.aveTimes"
 rm -f $output_file
 
 while [ "$start_date" != "$end_date" ]; do
@@ -13,6 +13,7 @@ while [ "$start_date" != "$end_date" ]; do
     start_date=$(date -d "$start_date +1 day" +"%Y%m%d")
 done
 
+echo "Count of the Months"
 line_count=$(wc -l < $output_file)
 num_years=$((line_count / 365))
 echo "The length of the file is: $line_count lines."
@@ -20,8 +21,11 @@ echo "This corresponds to approximately $num_years years."
 echo "________________________________________________________"
 
 ############# FREQ MonthLY #############
+start_date="19990101"
+end_date="20220101"
 
-output_file="Monthly_2.aveTimes"
+
+output_file="Monthly.aveTimes"
 rm -f $output_file
 
 while [ "$start_date" != "$end_date" ]; do
